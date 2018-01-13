3e3601f26a0125b9f7e1a0c97e2a2d713bd2ede5
潘 <piratf.me@gmail.com>
2018-01-13 18:14:00 +0800

    Update installvim8.sh
    更新脚本，解决编译缺少库的问题

--- installvim8.sh
+++ installvim8.sh
@@ -1,10 +1,23 @@
-yum -y install perl-devel perl-ExtUtils-Embed
-cd ~
-mkdir Download
-cd Download
-git clone git@github.com:vim/vim.git
+sudo yum install -y ruby ruby-devel lua lua-devel luajit \
+    luajit-devel ctags git python python-devel \
+    python3 python3-devel tcl-devel \
+    perl perl-devel perl-ExtUtils-ParseXS \
+    perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+    perl-ExtUtils-Embed
+git clone https://github.com/vim/vim.git
 cd vim
-./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-perlinterp --enable-cscope --prefix=/usr
-make
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=/usr/local
+make -j4
 sudo make install
 
